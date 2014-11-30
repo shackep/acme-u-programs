4aeb80f75fa6ace70051be0a7fce6c07b75fee5f
Shackelford <pshackel@administrators-MacBook-Pro.local>
2014-11-29 21:43:14 -0500

    added example repeater field

--- acme-u-programs.php
+++ acme-u-programs.php
@@ -162,7 +162,40 @@ function cmb2_sample_metaboxes( array $meta_boxes ) {
 			)
 		)
 	);
-
+	$meta_boxes['field_group'] = array(
+		'id'           => 'field_group',
+		'title'        => __( 'Program Emphases', 'cmb2' ),
+		'object_types' => array( 'acmeu_program', ),
+		'fields'       => array(
+			array(
+				'id'          => $prefix . 'repeat_group',
+				'type'        => 'group',
+				'description' => __( 'Method for listing multiple things to expect from this program. This may include approach/philosophy, learning environment or hands on esperience.', 'cmb2' ),
+				'options'     => array(
+					'group_title'   => __( 'Program Emphases {#}', 'cmb2' ), // {#} gets replaced by row number
+					'add_button'    => __( 'Add Another', 'cmb2' ),
+					'remove_button' => __( 'Remove this one', 'cmb2' ),
+					'sortable'      => true, // beta
+				),
+				// Fields array works the same, except id's only need to be unique for this group. Prefix is not needed.
+				'fields'      => array(
+					array(
+						'name' => 'Title',
+						'id'   => 'title',
+						'type' => 'text',
+						// 'repeatable' => true, // Repeatable fields are supported w/in repeatable groups (for most types)
+					),
+					array(
+						'name' => 'Description',
+						'description' => 'Write a short description for this entry',
+						'id'   => 'description',
+						'type' => 'textarea_small',
+					),
+				),
+			),
+		),
+	);
 	return $meta_boxes;
 }
+
 add_filter( 'cmb2_meta_boxes', 'cmb2_sample_metaboxes' );
